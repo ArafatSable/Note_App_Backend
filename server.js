e0300cd1a18f8263d06a8331ef6131e8c8d116a1
ArafatSable <95566091+ArafatSable@users.noreply.github.com>
2024-09-17 12:47:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ const app = express();
 const port = 5000;
 
 // origin: 'https://boisterous-pasca-6d8906.netlify.app', old
-// origin: 'https://scintillating-cactus-d9ad7e.netlify.app/',new
+// origin: 'https://hilarious-crepe-b8b2cf.netlify.app',new
 // Configure CORS to allow requests from your Netlify domain
 const corsOptions = {
-  origin: 'https://scintillating-cactus-d9ad7e.netlify.app/', // Replace with your Netlify domain
+  origin: 'https://hilarious-crepe-b8b2cf.netlify.app', // Replace with your Netlify domain
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true, // Enable sending cookies with requests
   allowedHeaders: 'Content-Type,Authorization',

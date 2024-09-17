ea62540ce2fee66d70ef46b01e741d3da9479cd5
ArafatSable <95566091+ArafatSable@users.noreply.github.com>
2024-09-17 10:28:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,10 +7,11 @@ require('dotenv').config();
 const app = express();
 const port = 5000;
 
-// origin: 'https://boisterous-pasca-6d8906.netlify.app', 
+// origin: 'https://boisterous-pasca-6d8906.netlify.app', old
+// origin: 'capable-florentine-190694.netlify.app',new
 // Configure CORS to allow requests from your Netlify domain
 const corsOptions = {
-  origin: 'capable-florentine-190694.netlify.app', // Replace with your Netlify domain
+  origin: 'https://capable-florentine-190694.netlify.app', // Replace with your Netlify domain
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true, // Enable sending cookies with requests
   allowedHeaders: 'Content-Type,Authorization',
